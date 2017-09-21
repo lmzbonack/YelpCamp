0a4ba6fbb7a00802bc7cd57e2c2cafa5ff13bb9b
Luc Zbonack <lmzbonack@gmail.com>
2017-09-21 21:06:08 +0000

    Changed to cloud database

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://luc:football66@ds147304.mlab.com:47304/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
